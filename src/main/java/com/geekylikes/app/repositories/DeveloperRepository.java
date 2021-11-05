d98fa194215bfa129e31728ec662689d98e0f56b
Eri <efwinckler@gmail.com>
2021-11-04 21:08:35 -0400

    progress done class 11-4-21

--- src/main/java/com/geekylikes/app/repositories/DeveloperRepository.java
+++ src/main/java/com/geekylikes/app/repositories/DeveloperRepository.java
@@ -1,7 +1,17 @@
 package com.geekylikes.app.repositories;
 
 import com.geekylikes.app.models.Developer;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
+
+    List<Developer> findAllByCohort(Integer cohort, Sort sort);
+
+//    @Query("SELECT * FROM developer WHERE cohort = ?1 AND ?2 in language")
+//    Developer findByCohortAndLanguage(Sort sort, Integer cohort,
+//                                      String language);
 }
